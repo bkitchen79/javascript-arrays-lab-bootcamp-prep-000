5ffddd84f29404b4b97099a1395019e154cee709
Bradley Kitchen <bradkitchen2004@sbcglobal.net>
2017-12-12 01:56:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 
-
-
-
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
 
 function removeFirstKitten() {
-  return kittens.slice(1, kittens.lenth);
+  return kittens.slice(1, kittens.length);
 }
